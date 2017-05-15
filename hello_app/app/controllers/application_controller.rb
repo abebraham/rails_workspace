13f094625cfca67ec385002e04907c0c3d56dd6b
Abraham <j.abraham.gomez@gmail.com>
2017-05-14 20:47:17 -0700

    move root hello_app

--- hello_app/app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  def goodbye
-  	msg = Time.now.to_s
-  	render html: "Goodbye homie! " + msg
-  end
-end
