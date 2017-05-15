0e2f150893afd71c5cbb131d77992b2064f1a602
Abraham <j.abraham.gomez@gmail.com>
2017-05-14 19:59:46 -0700

    Changed Readme

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def goodbye
+  	msg = Time.now.to_s
+  	render html: "Goodbye homie! " + msg
+  end
 end
